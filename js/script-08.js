1387827d85a951ea215eae4b43bf4266f800de4a
Jhon <example@gmail.com>
2022-03-29 09:24:32 +0300

    Добавлены три основных условия JS, узнал новое про brek & continue

--- js/script-08.js
+++ js/script-08.js
@@ -1,2 +1,25 @@
 "use strict";
 
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// };
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+
+for (let i = 1; i < 20; i++) {
+    // console.log(num);
+    // num++;
+
+    if (i % 2 !== 0) {
+        // break;   //! Останавливается
+        continue;   //! Пропускает нужное и идет дальше
+    }
+    console.log(i);
+};
