1b15e5acdfbbd05ae11155817767ca92baa99917
Jhon <example@gmail.com>
2022-04-04 12:44:08 +0300

    Изменения в конспекте урока 23 - ООП основы

--- js/script-17.js
+++ js/script-17.js
@@ -3,7 +3,7 @@
 let str = 'some';
 let strObj = new String(str);
 
-// console.log(typeof(str));
+// console.log(typeof(str)); //! typeof - тип данных
 // console.log(typeof(strObj));
 
 console.dir([1, 3, 4]);
@@ -16,15 +16,15 @@ const soldier = {
     }
 };
 
-const john = Object.create(soldier); //! Новый объект Джон прототипно наследуемый от слдата
+const john = Object.create(soldier); //! Новый объект Джон прототипно наследуемый от солдата
 
+john.health = 100;
 // const john = {
 //     health: 100
 // };
 
 // john.__proto__ = soldier; //! Устаревший способ прототипирования объекта
-
 Object.setPrototypeOf(john, soldier); //! Новый способ (прототип от Джона до солдата)
 
-// console.log(john.sayHello);
+console.log(john.health);
 john.sayHello()
