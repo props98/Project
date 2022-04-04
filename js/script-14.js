970950ac68954c0cc2cf7c4be4c0e14c54dab414
Jhon <example@gmail.com>
2022-04-04 11:32:22 +0300

    Конспектные дополнения для урока 20 – Objects

--- js/script-14.js
+++ js/script-14.js
@@ -15,19 +15,25 @@ const options = {
     }
 };
 
+console.log(options['colors']['bg']);
+
+// console.log(options);
+// delete options.name //! Удаление свойства обектов
+// console.log(options);
+
 const {border, bg} = options.colors; //! Деструктуризация объекта
 console.log(bg);
 
 // console.log(options.colors.bg);
 
-// delete options.name //! Удаление свойства обектов
-// console.log(options);
+
 
 let counter = 0; //! Создание счетчика
 
-for (let key in options) {  //! Перебор объектов
+//! Создание перебора объекта, в объекте при помощи условия
+for (let key in options) {  //! Перебор объектов – for in
 
-    if (typeof(options[key]) === 'object') {  //! <<<< Перебор объекта в объекте
+    if (typeof(options[key]) === 'object') {  //! <<<< Перебор объекта в объекте (typeof проверка типа данных)
         for (let i in options[key])
         console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
         counter++;
@@ -35,12 +41,13 @@ for (let key in options) {  //! Перебор объектов
         console.log(`Свойство ${key} имеет значение ${options[key]}`);
         counter++;
     }
-
 }
+
 console.log(counter);
 options.makeTest();
 
 //! Функции и методы внутри объекта
+//! Документация MDN – https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object
 
 console.log(Object.keys(options).length); //* Посчитать длину объекта
 
@@ -49,11 +56,10 @@ let animal = {
     color: 'red',
     hobby: 'play',
     eat: 5,
-    makeMetod: function() {
+    makeMetod: function() {  //! создание метода
         console.log('My first Metod in JS!');
     }
 }
-
 
 // console.log(animal.length);
 // console.log(Object.keys(animal)); //! Получение массива объекта
@@ -65,4 +71,29 @@ for (let key in animal) {
 }
 
 
+//! Деструктуризация объекта
+const deskObj = {
+    name: 'Mike',
+    age: 25,
+    obg1: {
+        nest1: 1,
+        red: {
+            nest2: 2,
+            blue: {
+                nest3: 3,
+                yellow: {
+                    nest4: 4,
+                    brown: 'End'
+                }
+            }
+        }
+    }
+};
 
+
+const {nest1, red} = deskObj.obg1;
+const {nest2, blue} = deskObj.obg1.red;
+const {nest3, yellow} = deskObj.obg1.red.blue;
+
+// console.log(deskObj);
+console.log(blue);
