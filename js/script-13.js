67ef30c4efec8e36eeebfa2067da924483cb17c1
Jhon <example@gmail.com>
2022-04-04 10:44:47 +0300

    Дополнен конспект к уроку 19 - Callback

--- js/script-13.js
+++ js/script-13.js
@@ -1,8 +1,10 @@
 "use strict";
 
+//! Callback Функции
+
 function first() {
     //Do something
-    setTimeout(function() {
+    setTimeout(function() { //! Залержка функции на пол секунды
         console.log(1);
     }, 1000);
 }
@@ -16,13 +18,23 @@ first();
 second();
 
 
-function done() {
-    console.log('Lesson is passed!');
-}
-
-function learnJS(lang, callback) {  //! Callback функция
+function learnJS(lang, callback) {  //! Callback функция / Первый аргумент текст (любой аргумент), второй важный аргумент CALLBACK (другая функция)
     console.log(`I learn: ${lang}`);
     callback();
 }
 
-learnJS('Java Script', done); //! Передовать без круглых скобок, чтобы вызываласб в опр. вермя.
+//! Ананимная функция / после вызова ищезнет
+learnJS('Java Script', function() {
+    console.log('I passed this lesson!');
+});
+
+//! Передача функции для дальнейшего её использования
+function done() {
+    console.log('Lesson is passed!');
+}
+
+function sayHello() {
+    console.log('Hello!');
+}
+
+learnJS('Java Script', sayHello); //! Передовать без круглых скобок, чтобы вызываласб в опр. вермя.
