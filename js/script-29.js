da3896564466bd9a78909f3e22d004c56e3e8ca0
Jhon <example@gmail.com>
2022-04-20 16:25:09 +0300

    Исправлено название скрипта, подправлен путь к скрипту 29

---
+++ js/script-29.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelector('heart');
+      oneHeart = document.querySelector('.heart');
+
+console.dir('box');
