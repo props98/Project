32ded92f0210c08ef557ca1c362b1aaa6859a66a
Jhon <example@gmail.com>
2022-04-20 19:20:37 +0300

    Сомостоятельная практика

--- js/script-29.js
+++ js/script-29.js
@@ -11,12 +11,12 @@ const box = document.getElementById('box'),
 // box.style.width  = '500px';
 
 //! Одиночное изменение стилей
-btns[1].style.borderRadius = '100%';
+// btns[1].style.borderRadius = '100%';
 circles[0].style.backgroundColor = 'yellow';
 
 //! Несколько - изменений стиля
-const num = 450;
-box.style.cssText = `background-color: red; width: ${num}px`;
+// const num = 450;
+// box.style.cssText = `background-color: red; width: ${num}px`;
 
 
 //! Действия над несколькими элементами 
@@ -27,15 +27,15 @@ box.style.cssText = `background-color: red; width: ${num}px`;
 // }
 
 hearts.forEach(item => {
-    item.style.backgroundColor = 'pink';
+    item.style.cssText = 'background-color: lightgrey';
 });
 
-const div = document.createElement('div');
+// const div = document.createElement('div');
 const text = document.createTextNode('Созданный текст через JS');
 
-div.classList.add('black');
+// div.classList.add('black');
 
-document.body.append(div); //? Элемент добавлен перед закрывающим тегом body
+// document.body.append(div); //? Элемент добавлен перед закрывающим тегом body
 
 // wrapper.append(div); //? Элемент вставлен в блок wrapper в конец
 // wrapper.appendChild(div); //! Устаревший способ добавление элемента
@@ -53,11 +53,41 @@ document.body.append(div); //? Элемент добавлен перед зак
 // hearts[0].replaceWith(circles[0]); //? замена выбранного элемента
 // wrapper.replaceChild(circles[0], hearts[0]); //! Устаревший способ замены элемента
 
-div.innerHTML = '<h1>Hello world!</h1>';
+// div.innerHTML = '<h1>Hello world!</h1>';
 // div.textContent = 'Hello!';
 
 // div.insertAdjacentHTML('afterBegin', '<h2>beforeBegin</h2>'); //? вставляется в начало/перед элементом h1
 // div.insertAdjacentHTML('beforeBegin', '<h2>afterBegin</h2>'); //? вставляет код перед элеменотом div
-div.insertAdjacentHTML('afterEnd', '<h2>afterEnd</h2>'); //? вставляет после элеменота div
+// div.insertAdjacentHTML('afterEnd', '<h2>afterEnd</h2>'); //? вставляет после элеменота div
 // div.insertAdjacentHTML('beforeEnd', '<h2>beforeEnd</h2>'); //? вставляется в в конце элементом h1
 
+
+//! Самостоятельная практика
+
+// box.style.marginBottom = '30px';
+
+box.style.cssText = 'border-bottom: 5px solid green; margin-bottom: 30px';
+
+let longText = btns[2].textContent;
+let sliced = longText.slice(0, 4);
+
+
+if (longText.length > sliced.length) {
+    sliced += '...';
+    console.log(sliced);
+    btns[2].textContent = sliced;
+}
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].style.cssText = 'background-color: blue; margin-bottom: 30px';
+}
+
+const wrapperCircle = document.createElement('div');
+
+for (let i = 0; i < circles.length; i++) {
+    circles[i].style.cssText = 'background-color: green;';
+    wrapperCircle.insertAdjacentHTML('afterbegin', circles);
+}
+
+circles[2].style.marginBottom = '30px';
