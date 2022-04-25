667c9c634c8ad354a66bb4ff7d41b16bb84d3245
Jhon <example@gmail.com>
2022-04-25 09:33:49 +0300

    Практические задачи повторены

--- project/js/script.js
+++ project/js/script.js
@@ -29,7 +29,8 @@ const movieDB = {
 //! Получение элементов со траницы
 const adv = document.querySelectorAll('.promo__adv img'),
       promoBg = document.querySelector('.promo__bg'),
-      genre = promoBg.querySelector('.promo__genre');
+      genre = promoBg.querySelector('.promo__genre'),
+      movies = document.querySelector('.promo__interactive-list');
 
 
 //! Удаление рекламных блоков со траницы
@@ -42,3 +43,13 @@ genre.textContent = 'Драма';
 
 //! Изменение заднего фона
 promoBg.style.cssText = `background-image: url("img/bg.jpg")`;
+
+movies.innerHTML = "";
+
+movieDB.movies.forEach((film, i) => {
+    movies.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
