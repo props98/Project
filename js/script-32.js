023b2edf662a7c72112d52d562b9cd906d6f38d1
Jhon <example@gmail.com>
2022-04-27 11:52:23 +0300

    Пройден урок

--- js/script-32.js
+++ js/script-32.js
@@ -1,2 +1,40 @@
 "use strict";
 
+
+//? Отталкиваемся от родителя
+// console.log(document.head);
+// console.log(document.documentElement);
+console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+//! Получение элемента 
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+
+//? Получение родителя от ребенка
+// console.log(document.querySelector('#current').parentNode);
+
+//! Получение элемента
+// console.log(document.querySelector('#current').parentElement);
+
+
+
+//? Получение через дата атрибута
+//! Получение узла
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+
+//! Получение элемента
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
