480244342d58ba735b27fcb63e1097a241c2d5d5
Jhon <example@gmail.com>
2022-04-28 21:13:22 +0300

    Repeat and correct lesson 32

--- js/script-32.js
+++ js/script-32.js
@@ -14,7 +14,7 @@ console.log(document.body.childNodes); //! Nodelist
 
 
 //? Получение родителя от ребенка
-// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode); //!Нода
 // console.log(document.querySelector('#current').parentNode.parentNode); //! Родитель родителя от ребенка
 
 //! Получение элемента
@@ -24,19 +24,18 @@ console.log(document.body.childNodes); //! Nodelist
 
 //? Получение через дата атрибута
 //! Получение узла
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextSibling); //!Нода
+// console.log(document.querySelector('[data-current="3"]').previousSibling); //!Нода
 
 //! Получение элемента
 // console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 // console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
 
-//!  Переберающая функция
+//!  Перебор НОД листа
 for (let node of document.body.childNodes) {
     if (node.nodeName == '#text') {
         continue;
     }
-
     console.log(node);
 }
