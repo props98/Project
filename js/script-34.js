66c520fa36b2ee3f74f84623f4a9a9caada3eb39
Jhon <example@gmail.com>
2022-05-01 19:35:47 +0300

    Начало урока

---
+++ js/script-34.js
@@ -0,0 +1,10 @@
+//? touchstart - касание экрана
+//? touchmove - смещение пальца
+//? touchend - потравание пальца от экрана
+//? touchenter -
+//? touchleave - 
+//? touchcancel - точка не регистрируется
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+});
