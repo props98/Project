05f0b41340500a832b2eaa1860b743b061524312
Jhon <example@gmail.com>
2022-05-01 19:47:51 +0300

    Урок 34 пройден

--- js/script-34.js
+++ js/script-34.js
@@ -6,5 +6,29 @@
 //? touchcancel - точка не регистрируется
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('Hello');
+        console.log(e.targetTouches);
+    });
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('end');
+    // });
+
 });
+
+//? touches - список всех пальцев которые заимодействуют с экраном
+//? targetTouches - 
+//? changedTouches
