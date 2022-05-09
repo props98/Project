39933d1e80c53c8345b5f359cdb1e1251c1089bb
Jhon <example@gmail.com>
2022-05-09 17:08:43 +0300

    Нахождение элементов влакдок на странице

--- video-038/js/script.js
+++ video-038/js/script.js
@@ -2,6 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     
+    //! нходим вкладки на которые мы будем кликать
+    const tabs = document.querySelectorAll('.tabheader__item');
+    //! находим контент вкладки
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    //! находим рабителя всех вкладок
+    const tabsParent = document.querySelector('.tabheader__items');
 
+    
 
 });
