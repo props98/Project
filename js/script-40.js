1b77c4c52fed595a9b463ccbe036321fd9f9d8ec
Jhon <example@gmail.com>
2022-05-16 18:22:30 +0300

    Создан поробный конспект: Тема new Date()

--- js/script-40.js
+++ js/script-40.js
@@ -6,6 +6,7 @@
 
 //!Передача даты в ввиде строки
 const now1 = new Date('2022-05-15');
+//? new Date.parse('2022-05-15');
 
 //! Передача прямых аргументов
 const now2 = new Date(2020, 5, 15);
@@ -14,44 +15,46 @@ const now2 = new Date(2020, 5, 15);
 //* Чтобы получить ранюю дату нужно использвать отрицытельные значения -777
 const now3 = new Date(0);
 
-//? new Date.parse('2022-05-15');
-
 //!Вывод текущей системной даты
 // console.log(now);
 
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getHours());
-// console.log(now.getMinutes());
-// console.log(now.getSeconds());
-
-// console.log(now.getDay()); //! Начало с воскресенья = 0
+//! Основные Методы по отдельным компонентам
+// console.log(now.getFullYear()); //? получаем год всегда четырех значный
+// console.log(now.getMonth()); //? получаем месяц - счет с нуля 0 - 11
+// console.log(now.getDate()); //? дни от 1 до 31
+// console.log(now.getDay()); //? Начало с воскресенья = 0
 
 // console.log(now.getHours());
-// console.log(now.getUTCHours());
+// console.log(now.getMinutes();
+// console.log(now.getSeconds();
 
+//! Местное время
+// console.log(now.getHours());
+//! Аналог UTC - 
+// console.log(now.getUTCHours());]
+
+//! Метод - разница между часовым поясом и UTC
 // console.log(now.getTimezoneOffset());
 
+//! Колчиество милисекунд которые прошли с начальной точки отсчета
 // console.log(now.getTime());
+
 // let time = now.getTime();
 // console.log(Date(time));
 
-//! Сеттеры
-
+//! Сеттеры - установка времени
 // console.log(now.setHours(11));
 // console.log(now.setDate(11));
 // console.log(now.setMonth(10));
 // console.log(now.setYear(2011));
-// console.log(now);
 
-//! Простенький Benchmark
-let start = new Date();
+//! Простенький Benchmark - Посмотреть производительность системы
+let start = new Date(); //? Текущая дата
 
 for (let i = 0; i < 100000; i++) {
-    let some = i ** 3; //! Возводит в степень
+    let some = i ** 3; //? ** Возводит в степень
 }
 
-let end = new Date();
+let end = new Date(); //? Конечная дата
 
 alert(`Отработал за ${end - start} миллисекунд`);
