5c16d4ccdbbe08c5bfe586e3a16b2cff22768d7d
Jhon <example@gmail.com>
2022-05-23 15:57:07 +0300

    Повторение урока 42

--- js/script-42.js
+++ js/script-42.js
@@ -3,25 +3,52 @@
 const box = document.querySelector('.box'),
       btn = document.querySelector('button');
 
+//? Получение ширины и высоты видимой части элемента не включая margin (border-box) - 15px(Колесо прокрутки)
+//? 385 – 235px
 // const width = box.clientWidth;
 // const height = box.clientHeight;
 
-const width = box.offsetWidth;
+//? Получение ширины и высоты видимой части элемента включая margin // 400 – 250px
+// const width = box.offsetWidth;
 // const height = box.offsetHeight;
 
+//? Полная высота элемента включая невидимую часть элемента
+//? Минус 15px(Колесо прокрутки) у ширины элемента 
+const width = box.scrollWidth;
 const height = box.scrollHeight;
 
 console.log(width, height);
 
+//? при нажатии на кнопку элемент полностью раскрывается
 btn.addEventListener('click', () => {
     // box.style.height = box.scrollHeight + 'px';
+
+    //? При нажатии на кнопку показывает в консоль на сколько пикселей с верху
+    //? спустился скролл
     console.log(box.scrollTop);
 });
 
-console.log(box.getBoundingClientRect().top);
+//? Метод которое получает все координаты которые есть у элемента (Верх, лево, право, низ)
+console.log(box.getBoundingClientRect());
+// console.log(box.getBoundingClientRect().top);
+// console.log(box.getBoundingClientRect().height);
 
-const style = window.getComputedStyle(box);
+//? Метод узнает какие стили были применены к элементу
+const style = window.getComputedStyle(btn);
+
+//? Получение всего списка стилей
+console.log(style);
+//? получение свойста display
 console.log(style.display);
+//? Получение цвета фона
+console.log(style.backgroundColor);
+//? Получение свойства выравнивания
+console.log(style.alignItems);
 
-
+console.dir(document);
 console.log(document.documentElement.scrollTop);
+
+//? Скролл относительно текущей позиции
+// window.scrollBy(0, 400)
+//? Скролл относительно всей страницы
+// window.scrollTo(0, 400)
