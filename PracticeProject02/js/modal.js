4c18d753af99572bd47f62cc5fad47061d6f8098
Jhon <example@gmail.com>
2022-05-23 17:15:14 +0300

    Пройден урок 43

--- PracticeProject02/js/modal.js
+++ PracticeProject02/js/modal.js
@@ -1,2 +1,50 @@
 "use strict";
 
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTrigger = document.querySelectorAll('[data-modal'),
+          modal = document.querySelector('.modal'),
+          closeBtn = document.querySelector('[data-close]');
+
+
+    // modalTrigger.forEach(btn => {
+    //     btn.addEventListener('click', () => {
+    //         modal.style.cssText = 'display: block';
+    //     });
+
+    //     closeBtn.addEventListener('click', () => {
+    //         modal.style.cssText = 'display: none';
+    //     })
+    // });
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    closeBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+});
