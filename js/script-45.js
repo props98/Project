931772c95ef5e77b0f74bbdce83f286ffba87be2
Jhon <example@gmail.com>
2022-06-01 08:51:30 +0300

    Повторение урока 45

--- js/script-45.js
+++ js/script-45.js
@@ -3,31 +3,38 @@
 const num = new Number(3);
 console.log(num);
 
+//* Конструктор User
 function User(name, id) {
     this.name = name;
     this.id = id;
     this.human = true;
-    
-    hello() {
+
+    //* Создание метода объекта
+    this.hello = function() {
         console.log(`Hello ${this.name}`);
-    }
-    exit() {
-        console.log(`User ${this.name} left`);
-    }
+    };
+
+    // exit() {
+    //     console.log(`User ${this.name} left`);
+    // }
 }
 
 
-
-// User.prototype.exit = function(name) {
-//     console.log(`Пользователь ${this.name} ушел!`);
-// }
+//*Свойство prototype с помощью него можно добовлять методы в конструктор
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел!`);
+}
 
 const ivan = new User('Ivan', 28);
 const alex = new User('Alex', 30);
 
+// console.log(ivan);
+// console.log(alex);
+
 // ivan.exit();
 
-// ivan.hello();
-// alex.hello();
+ivan.hello();
+alex.hello();
+ivan.exit();
 
 console.log(ivan.id);
