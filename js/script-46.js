8f79bfb29a00d770d0fd25373388588c5a1a5ed4
Jhon <example@gmail.com>
2022-06-03 17:54:17 +0300

    Повторение урока 46 котекстные вызовы

--- js/script-46.js
+++ js/script-46.js
@@ -3,23 +3,36 @@
 //* Контекст вызова функции "this"
 //* То что окружает функцию и что вызывается
 
-//? Первый вызов функции
+//? 1) 
+//? Первый вызов функции / Простой вызов функции
+//! Обычная функция:  this = window, но если стоит "use strict"  this = undefined
+
+//* function showThis() {
+//*     console.log(this);
+//* }
+//* showThis();
+
 // function showThis(a, b) {
 //     console.log(this);
 //     function sum() {
 //         console.log(this);
 //         return a + b;
 //     }
-
 //     console.log(sum());
 // }
 // showThis(4, 5);
 
+//? 2)
 //? Второй вызов функции
+//! Контекст у методов объекта это будет сам объект
+
 // const obj = {
 //     a: 20,
 //     b: 15,
 //     sum: function() {
+//         //  console.log(this);
+
+//         //? Простой вызов фнкции, контекст вызова в этой функции выдаст undefined
 //         function shout() {
 //             console.log(this);
 //         }
@@ -28,19 +41,28 @@
 // }
 // obj.sum();
 
+//? 3)
 //? Третий вызов функции - конструктора
+//! This в конструктарах и классах это новый экземпляр объекта / This ссылаеться на новый объект
+
 // function User(name, id) {
 //     this.name = name;
 //     this.id = id;
 //     this.human = true;
+
+//     //? Метод объекта
 //     this.hello = function() {
 //         console.log(`Hello! ${this.name}`);
 //     }
 // }
 // let ivan = new User('Ivan', 23);
+
 // console.log(ivan.hello());
 
+//? 3)
 //? Четвертый вызв функции
+//! РУчная привязка this: call, apply, bind
+
 // function sayName(surname) {
 //     console.log(this);
 //     console.log(this.name + surname);
@@ -50,14 +72,16 @@
 //     name: 'Jhon'
 // };
 
+//! Первый вариант ручного присвоения контекста метод - call()
 //? добавление аргумента через запятую
 // sayName.call(user, ' Smith');
 
+//! Второй вариант ручного присвоения контекста, метод - apply()
 //? добавление аргумента через массив
 // sayName.apply(user, [' Arrr']);
 
-
-//? Создает новую функцию связанную с определеным контестом
+//! Третий вариант ручного присвоения контекста, метод - bind()
+//! Создает новую функцию связанную с определеным контекстом
 //  function count(num) {
 //      return this * num;
 //  } 
@@ -68,45 +92,56 @@
 
 
 
-//! Обычная функция:  this = window, но если стоит use strict  this = undefined
-//! контекст у методов объекта это будет сам объект
-//! This в конструктарах и классах это новый экземпляр объекта
-//! РУчная привязка this: call, apply, bind
-
-
 const btn = document.querySelector('button');
 
 //? при класческом вызове функции при клике будет ссылаться на сам элемент
 // btn.addEventListener('click', function() {
-//     this.style.backgroundColor = 'red';
+//     // console.log(this);
+//     this.style.backgroundColor = 'yellow';
 // });
 
-//? Стрелочная функция
+//? Стрелочная функция Контекст вызова теряеться
 btn.addEventListener('click', (e) => {
-    e.target.style.backgroundColor = 'red';
+    e.target.style.backgroundColor  = 'yellow';
 });
 
 //? Функция в объекте ссылается на сам объект
 //? стрелочная функция ссылается на родителя
-//? стрелочные функции не имеют своего контекста вызова
+//? стрелочные функции не имеют своего контекста вызова "this"
 const obj = {
     num: 5,
+    text: 'Hello',
     sayNumber: function() {
+        //? Стрелочная функция ссылаеться на родителя
         const say = () => {
-            console.log(this.num);
+            console.log(this.text);
         }
-
         say();
     }
+
+    // sayNumber: function() {
+    //     console.log(this);
+    //     function sayText() {
+    //         //! Обычный вызов функции выдасть undefind
+    //         console.log(this);
+    //     }
+
+    //     sayText();
+    // }
 }
 
 obj.sayNumber();
+
 
 const double1 = (a) => {
     return a * 2;
 };
 
-const double2 = (a) => a * 2;
+//? Короткие виды записи
+//! return не нужен
+// const double2 = (a) => a * 2;
+
+//! Если один аргумент скобки можно не использовать
 const double3 = a => a * 2;
 
 console.log(double1(4));
