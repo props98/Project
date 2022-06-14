b125f14616c1a2210b7a5ec4ce3239db83bff8bf
Jhon <example@gmail.com>
2022-06-14 12:40:49 +0300

    Дополнение конспекта небольшие примеры

--- js/script-49.js
+++ js/script-49.js
@@ -13,3 +13,54 @@ function calcOrDouble(number, basis = 2) {
 }
 
 calcOrDouble(3);
+
+// Остаточные параметры и оператор расширения
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+console.log(sum(1, 2, 3, 4, 5));
+
+
+function sumAll(...args) {
+    let sum = 0;
+
+    for (let arg of args) {
+        sum += arg;
+    }
+    return sum;
+}
+console.log(sumAll(1, 2, 3));
+
+function showName(firstName, lastName, ...titles) {
+    console.log(firstName + ' ' + lastName);
+    console.log(titles.length);
+    console.log(titles[0]);
+    console.log(`${titles[0]} ${firstName} ${lastName}`);
+}
+showName('Ivan', 'Petrov', 'Hello', 'Bye');
+
+function showArguments() {
+    console.log(arguments.length);
+    console.log(arguments[0]);
+}
+showArguments('Hello', 'world');
+
+
+// Оператор расширения
+
+
+console.log(Math.max(3, 5, 1));
+
+let arr = [3, 2, 1];
+console.log(...arr, 10);
+console.log(Math.max(...arr, 10));
+
+let arr1 = [1, 2, 3];
+let arr2 = [5, 6, 7];
+let merge = [0, ...arr1, 0, ...arr2];
+console.log(merge);
+
+let str = 'Hello';
+console.log([...str][1]);
+console.log(Array.from(str)[2]);
