5d28b8d99b5e901a5d73ae6a8de2a3949ba1b4dc
Jhon <example@gmail.com>
2022-06-20 13:31:04 +0300

    Повторение урока, дополнение конспекта

--- js/script-51.js
+++ js/script-51.js
@@ -17,13 +17,18 @@ const persone = {
 //! 2 - form Data 
 //! 3 - формат Data JSON 
 
+//* Превращает объект в формат JSON >>>> Все сущности записаны в двойные скобки
+//* В таком формате можно отправить на сервер
 // console.log(JSON.stringify(persone));
-// console.log(JSON.parse(JSON.stringify(persone)));
+
+//* Форматирование формата JSON в обычный объект JS
+//* Можно использовать во фронттенде
+//console.log(JSON.parse(JSON.stringify(persone)));
 
 //! Глубокоу клонирование объекта не зависящие друг от друга
-let clone = JSON.parse(JSON.stringify(persone))
-
+let clone = JSON.parse(JSON.stringify(persone));
 clone.parents.mom = 'Ann';
+clone.parents.dad = 'Pete';
 
 console.log(persone);
 console.log(clone);
