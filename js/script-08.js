8bbaad2cb1338912701bb7ec862cabbf88702003
Max <mp@Mbp.local>
2022-06-26 13:23:14 +0300

    Начало конспекта

--- js/script-08.js
+++ js/script-08.js
@@ -1,27 +1,6 @@
 "use strict";
 
-let num = 50;
+//* Циклы
 
-//! Проверяет условие затем выполняет действие
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// };
+const num = 50;
 
-//! Сначала выполняет действие затем проверяет условие
-// do {
-//     console.log(num);   
-//     num++;
-// }
-// while (num <= 55);
-
-for (let i = 1; i < 20; i++) {
-    // console.log(num);
-    // num++;
-
-    if (i % 2 !== 0) {
-        // break;   //! Останавливается
-        continue;   //! Пропускает нужное и идет дальше
-    }
-    console.log(i);
-};
