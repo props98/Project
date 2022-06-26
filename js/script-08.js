e0374ac64254be33a414ef40115884281d02eb79
Jhon <mp@Mbp.local>
2022-06-26 14:27:49 +0300

    Повтор урока 14

--- js/script-08.js
+++ js/script-08.js
@@ -4,8 +4,40 @@
 
 let num = 50;
 
-// Первый цикл 
-while (num <= 55) {
-    console.log(num);
-    num++;
+//* Первый цикл 
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+//* Второй цикл
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+//* Третий цикл
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+//* Цикл с отановкой «break» / «continue»
+for (let i = 1; i < 10; i++) {
+    if (i  === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
+
+//////////////////? Задачи
+
+//* Перебор четных цифр от 1 - 30
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(i);
 }
