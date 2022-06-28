51e86b44ce9ce3249d71f6e80c9427fcef790069
Jhon <mp@Mbp.local>
2022-06-28 09:18:42 +0300

    repeat video lesson 19

--- js/script-13.js
+++ js/script-13.js
@@ -1,4 +1,18 @@
 "use strict";
 
-//! Callback Функции
+//* Callback Функции
 
+//Функция с задержкой, имитация работы функции с сервером
+function first() {
+    // do something
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
