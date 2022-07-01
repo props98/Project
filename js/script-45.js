ae354b10c6f082282a57fcd6974ad231ee1aff5c
Jhon <jhon@mail.com>
2022-07-01 13:35:22 +0300

    Подготовка к повторению урока 45

--- js/script-45.js
+++ js/script-45.js
@@ -1,36 +1,3 @@
 "use strict";
 
-const num = new Number(3);
-console.log(num);
-
-//* Конструктор User
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-
-    //* Создание метода объекта
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-
-}
-
-
-//*Свойство prototype с помощью него можно добовлять методы в конструктор
-//! Добавлен метод exit
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел!`);
-}
-
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 30);
-
-console.log(ivan.id);
-
-// console.log(ivan);
-// console.log(alex);
-
-ivan.hello();
-alex.hello();
-ivan.exit();
+//* Функции конструкторы
