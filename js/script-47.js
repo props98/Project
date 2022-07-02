aa0b1c85c2d674fb93dfb8675895e1337b208502
Jhon <jhon@mail.com>
2022-07-02 20:58:01 +0300

    Пройдено повторение урока 47

--- js/script-47.js
+++ js/script-47.js
@@ -1,3 +1,42 @@
 'use strict';
 
-//* Классы - в стандарте ES6
+//* 2015год -> Классы - в стандарте ES6
+//* Синтактический сахар
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+//* Наследование свойств и методов от класса «Rectangle»
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        //* Метод который вызывает конструктор родителя «Rectangle»
+        //! Должна быть всегда на первом месте в конструкторе при раскрытии конструктора
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello', 'yellow');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
