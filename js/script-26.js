ca4fea7820eebf469423d65d0001003fc180df4d
Jhon <jhon@mail.com>
2022-07-05 15:56:23 +0300

    Подготовка к повторению урока 26

--- js/script-26.js
+++ js/script-26.js
@@ -3,57 +3,11 @@
 //? Динамическая типизация
 
 //! to String
-//1
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
 
-//2
-console.log(typeof(5 + ''));
-console.log(typeof(null + ''));
-
-const num = 5;
-console.log('https://vk.com/catalog/' + num); // https://vk.com/catalog/5
-
-const fontSize = 26 + 'px';
-console.log(fontSize); // 26px
 
 //!To Number
-//1
-console.log(typeof(Number('4')));
 
-//* 2 Унарный плюс
-console.log(typeof(+'5'));
-
-// 3
-console.log(typeof(parseInt('15px', 10)));
-
-
-let answer = +prompt('Hello', '');
 
 
 //! to Bolean
 
-//! всегда false
-0, '', undefined, Nan;
-
-//? 1 Нативный способ динамического преоброзования
-
-let switcher = null;
-
-if (switcher) {
-    console.log('working...'); //? Не выполниться null = False
-}
-
-switcher = 1;
-
-if (switcher) {
-    console.log('working...'); //? Выполниться 1 = True
-}
-
-//? 2 способ
-
-console.log(typeof(Boolean('4')));
-
-//? 3 способ
-
-console.log(typeof(!!'22222'));
