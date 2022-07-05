d206ecb6bbad970ad7fecee9eb1aae5a5098a5ef
Jhon <jhon@mail.com>
2022-07-05 16:23:26 +0300

    Повторение урока 26

--- js/script-26.js
+++ js/script-26.js
@@ -2,12 +2,53 @@
 
 //? Динамическая типизация
 
-//! to String
+//////////////* to String
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2) Конкaтенация
+console.log(typeof(String(5 + '')));
+console.log(typeof(String(null + '')));
+
+const num = 5;
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
 
 
-//!To Number
+//////////////* To Number
+// 1)
+console.log(typeof(Number('4')));
+
+// 2) Унарный плюс
+console.log(typeof(+'4'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt('Hello', '');
 
 
+//////////////* to Bolean
+// 1)
+//! Всeгда false 
+//!   0, '', null, undefined, NaN
 
-//! to Bolean
+//* Не работает switcher => null => false
+let switcher = null;
+if (switcher) {
+    console.log('Working...');
+}
 
+//* Работает switcher => 1 => true
+switcher = 1;
+if (switcher) {
+    console.log('Working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3) Два знака отрицания превращает в boolean
+console.log(typeof((!!'4444')));
