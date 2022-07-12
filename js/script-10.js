9e4f77ffc4151f39320fbfc67985d098a6f0050c
Jhon <jhon@mail.com>
2022-07-12 15:36:54 +0300

    Пройденно повторение урока 16

--- js/script-10.js
+++ js/script-10.js
@@ -4,7 +4,7 @@
 //* Function declaration -
 //* Создается до начала выполнения скрипта, можно вызвать перед объявлением
 //?  function foo() {
-//?  ...
+//?     ...
 //?  }
 
 //* Function expression - 
@@ -20,4 +20,39 @@
 //?      ...
 //?  };
 
+let num = 20;
 
+function showFirstMessage(text) {
+    console.log(text);
+    // let num = 10;
+    console.log(num);
+}
+showFirstMessage('Hello world!');
+console.log(num);
+
+
+function calc(a, b) {
+    return (a + b);
+    // console.log('dew'); // После return код не выполняеться
+}
+console.log(calc(2, 3));
+console.log(calc(7, 3));
+console.log(calc(7, 13));
+
+function ret() {
+    let num = 50;
+
+    // какой-то код
+
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function() {
+    console.log('Hello');
+};
+logger();
+
+const calc2 = (a, b) => a + b;
