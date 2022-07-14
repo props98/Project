240bb7ad539f3c862a4cd854a839b965f33c174f
Jhon <jhon@mail.com>
2022-07-14 17:19:03 +0300

    Повторение урока 45 - функции конструкторы ES - 5

--- js/script-45.js
+++ js/script-45.js
@@ -1,4 +1,37 @@
 // "use strict";
 
-//* Функции конструкторы ES - «5»
+//* Функции конструкторы стандарты ES - «5»
 
+//! Устаревший вид
+const num = new Function(3);
+console.log(num);
+
+//! Функция конструктор - конструирование объектов
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    //* Метод
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+//! Добавление метода через прототип
+User.prototype.exit = function(name) {
+    console.log(`User ${this.name} left`);
+};
+
+
+const ivan = new User('Ivan', 28);
+const max = new User('Max', 20);
+
+ivan.exit();
+
+ivan.hello();
+max.hello();
+
+console.log(ivan);
+console.log(max);
+
+//! Классы - синтактический сахар ES - 6
