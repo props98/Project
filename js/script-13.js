2972d0fceeaa5140bedcd3df8a5f0103a57628eb
Jhon <jhon@mail.com>
2022-07-21 21:16:23 +0300

    Повтор урока 19

--- js/script-13.js
+++ js/script-13.js
@@ -2,3 +2,37 @@
 
 //* Callback Функции
 
+function first(second) {
+    //DO Something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+    second()
+}
+
+function second() {
+    console.log(2);
+}
+
+first(second);
+
+// function line(a, b) {
+//     console.log(1);
+//     a();
+//     console.log(2);
+//     b();
+// }
+// line(first, second);
+
+
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I pass this lesson!');
+}
+
+learnJS('JavaScript', done);
