84dec74ff4c6158107be285bf07cb96a4e0bde15
Jhon <jhon@mail.com>
2022-07-21 21:01:38 +0300

    Подготовка к повторению урока 19

--- js/script-13.js
+++ js/script-13.js
@@ -2,30 +2,3 @@
 
 //* Callback Функции
 
-//Функция с задержкой, имитация работы функции с сервером
-function first() {
-    // do something
-    setTimeout(function () {
-        console.log(1);
-    }, 500);
-}
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log('I learn ' + lang);
-    callback();
-}
-
-function done() {
-    console.log('I passed this lesson');
-}
-
-//Передаем функцию не вызывая ее, функция вызывается в «learnJS»
-learnJS('JavaScript', done);
-
